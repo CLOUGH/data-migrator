4d86d450e23ea06898266a1601063bf16826e2de
Warren Clough <clough.warren@gmail.com>
2018-02-13 20:53:59 -0500

    added table component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgxElectronModule } from 'ngx-electron';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatTabsModule,
+  MatCheckboxModule,
+  MatButtonModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatRadioModule,
+  MatSelectModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
     NgxElectronModule,
     BrowserAnimationsModule,
-    MatButtonModule, MatCheckboxModule
+    MatTabsModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule
+  ],
+  exports: [
+    MatTabsModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
