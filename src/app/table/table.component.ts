721d9de32f6cf46cda4ec6be0fccd904f346451f
Warren Clough <clough.warren@gmail.com>
2018-02-15 13:57:19 -0500

    change angular to use scss. fixed issue with loading data from the electron.

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,26 +1,30 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, NgZone } from '@angular/core';
 import { ElectronService } from 'ngx-electron';
 import { Mapping } from '../../electron/migrator/interfaces/mapping.interface';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
 
   @Input() public table: string;
-  public mapping: Mapping;
+  public mapping: any;
 
-  constructor(private electronService: ElectronService) { }
+  constructor(private electronService: ElectronService, private zone: NgZone) { }
 
   ngOnInit() {
-    console.log('reach');
     this.electronService.ipcRenderer.send('table.getMapping', { table: this.table });
+    this.mapping = { test: 'test' };
     this.electronService.ipcRenderer.on('table.mapping', (event, args) => {
-      console.log(args);
-      this.mapping = args;
+      this.zone.run(() => {
+        this.mapping = args;
+      });
     });
+  }
+
+  updateMapping(mapping) {
   }
 
 }
